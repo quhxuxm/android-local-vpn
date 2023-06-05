5804ba156f4f93d204497dfa8f4ad70e06795bb5
quhxuxm <quhxuxm.quh@gmail.com>
2023-06-05 13:43:33 +0800

    study

---
+++ source/android/app/src/main/java/com/github/jonforshort/androidlocalvpn/vpn/LocalVpnConfiguration.kt
@@ -0,0 +1,14 @@
+package com.github.jonforshort.androidlocalvpn.vpn
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class LocalVpnConfiguration(
+    val allowedApps: List<PackageName>? = null,
+    val disallowedApps: List<PackageName>? = null
+) : Parcelable
+
+@JvmInline
+@Parcelize
+value class PackageName(val packageName: String) : Parcelable
