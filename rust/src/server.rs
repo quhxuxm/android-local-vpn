fe566c4eca22957a8c67524adc075b7adf842b42
quhxuxm <quhxuxm.quh@gmail.com>
2023-08-02 16:22:51 +0800

    refact

--- rust/src/server.rs
+++ rust/src/server.rs
@@ -1,24 +1,19 @@
-use std::fs::File;
+use std::{fs::File, sync::Arc};
 
 use std::{
     collections::{hash_map::Entry, HashMap},
     io::{ErrorKind, Read},
     os::fd::FromRawFd,
-    sync::Arc,
 };
 
 use anyhow::Result;
 use anyhow::{anyhow, Error as AnyhowError};
-use log::{debug, error, info, trace};
-use tokio::sync::mpsc::Receiver;
+use log::{debug, error, info};
+
 use tokio::task::yield_now;
 use tokio::{
     runtime::{Builder as TokioRuntimeBuilder, Runtime as TokioRuntime},
-    sync::{
-        mpsc::error::TryRecvError,
-        mpsc::{channel, Sender},
-        Mutex,
-    },
+    sync::{mpsc::Sender, Mutex},
     task::JoinHandle,
 };
 
@@ -32,7 +27,7 @@ use crate::{
 pub(crate) struct PpaassVpnServer {
     config: &'static PpaassVpnServerConfig,
     file_descriptor: i32,
-    stop_sender: Option<Sender<bool>>,
+    closed: Arc<Mutex<bool>>,
     runtime: Option<TokioRuntime>,
     processor_handle: Option<JoinHandle<Result<()>>>,
     transports: Arc<Mutex<HashMap<TransportId, Sender<Vec<u8>>>>>,
@@ -43,7 +38,7 @@ impl PpaassVpnServer {
         Self {
             config,
             file_descriptor,
-            stop_sender: None,
+            closed: Arc::new(Mutex::new(false)),
             runtime: None,
             processor_handle: None,
             transports: Default::default(),
@@ -66,16 +61,14 @@ impl PpaassVpnServer {
 
         let client_file = Arc::new(Mutex::new(unsafe { File::from_raw_fd(file_descriptor) }));
         let (client_file_read, client_file_write) = (Arc::clone(&client_file), client_file);
-        let (stop_sender, stop_receiver) = channel::<bool>(1);
-        self.stop_sender = Some(stop_sender);
         let transports = Arc::clone(&self.transports);
         let ppaass_server_config = self.config;
-
+        let closed = Arc::clone(&self.closed);
         let processor_handle = runtime.spawn(async move {
             info!("Begin to handle client file data.");
             Self::start_handle_client_data(
                 client_file_read,
-                stop_receiver,
+                closed,
                 transports,
                 client_file_write,
                 agent_rsa_crypto_fetcher,
@@ -92,7 +85,7 @@ impl PpaassVpnServer {
 
     async fn start_handle_client_data(
         client_file_read: Arc<Mutex<File>>,
-        mut stop_receiver: Receiver<bool>,
+        closed: Arc<Mutex<bool>>,
         transports: Arc<Mutex<HashMap<TransportId, Sender<Vec<u8>>>>>,
         client_file_write: Arc<Mutex<File>>,
         agent_rsa_crypto_fetcher: &'static AgentRsaCryptoFetcher,
@@ -100,12 +93,10 @@ impl PpaassVpnServer {
     ) {
         let mut client_file_read_buffer = [0u8; 65536];
         loop {
-            match stop_receiver.try_recv() {
-                Ok(true) => break,
-                Ok(false) => error!("Receive a unexpected stop flag."),
-                Err(TryRecvError::Disconnected) => break,
-                Err(TryRecvError::Empty) => {
-                    trace!("No stop flag.")
+            {
+                let closed = closed.lock().await;
+                if *closed {
+                    break;
                 }
             }
             let client_data = {
@@ -188,13 +179,11 @@ impl PpaassVpnServer {
     pub(crate) fn stop(&mut self) -> Result<()> {
         debug!("Stop ppaass vpn server");
         if let Some(runtime) = self.runtime.take() {
-            if let Some(stop_sender) = self.stop_sender.take() {
-                runtime.spawn(async move {
-                    if let Err(e) = stop_sender.send(true).await {
-                        error!("Fail to send stop command to ppaass server because of error: {e:?}")
-                    };
-                });
-            }
+            let closed = Arc::clone(&self.closed);
+            runtime.spawn(async move {
+                let mut closed = closed.lock().await;
+                *closed = true;
+            });
         }
         let processor_handle = self
             .processor_handle
