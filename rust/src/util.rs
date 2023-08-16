b1cf1bacdd48f64b1613ceb97afb1b1ffa9cd617
quhxuxm <quhxuxm.quh@gmail.com>
2023-08-16 08:17:34 +0800

    refact

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -6,18 +6,6 @@ use ppaass_common::{PpaassMessagePayloadEncryptionSelector, RsaCrypto, RsaCrypto
 use crate::{error::RemoteEndpointError, transport::TransportId};
 use crate::{JAVA_VPN_JVM, JAVA_VPN_SERVICE_OBJ};
 
-// use smoltcp::wire::{Ipv4Packet, Ipv6Packet, PrettyPrinter};
-
-// pub(crate) fn log_ip_packet(data: &[u8]) -> String {
-//     match Ipv4Packet::new_checked(&data) {
-//         Ok(ipv4_packet) => PrettyPrinter::print(&ipv4_packet).to_string(),
-//         Err(ipv4_err) => match Ipv6Packet::new_checked(&data) {
-//             Ok(ipv6_packet) => PrettyPrinter::print(&ipv6_packet).to_string(),
-//             Err(ipv6_err) => format!("{ipv4_err:?} || {ipv6_err:?}"),
-//         },
-//     }
-// }
-
 pub(crate) fn protect_socket(transport_id: TransportId, socket_fd: i32) -> Result<(), RemoteEndpointError> {
     trace!("Begin to protect outbound socket: {socket_fd}");
     let socket_fd_jni_arg = JValue::Int(socket_fd);
